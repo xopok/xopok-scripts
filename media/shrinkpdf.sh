1def67a43edece660a751855fc20c2714bc691d0
Vitaly Lysenkov <vlysenkov@mail.ru>
2014-02-28 22:07:19 +0100

    An utility to crop every page of pdf to its contents

---
+++ media/shrinkpdf.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+TMPFILE=/dev/shm/pdfshrink.pdf
+rm $TMPFILE
+
+TMPLINK=/dev/shm/pdfcrop.link.pdf
+SRCFILE=`realpath "$2"`
+
+echo ln -sf "$SRCFILE" $TMPLINK
+ln -sf "$SRCFILE" $TMPLINK
+
+pdfcrop $TMPLINK $TMPFILE
+
+gs  -q -dNOPAUSE -dBATCH -dSAFER \
+    -sDEVICE=pdfwrite \
+    -dCompatibilityLevel=1.3 \
+    -dPDFSETTINGS=/screen \
+    -dEmbedAllFonts=true \
+    -dSubsetFonts=true \
+    -dColorImageDownsampleType=/Bicubic \
+    -dColorImageResolution=72 \
+    -dGrayImageDownsampleType=/Bicubic \
+    -dGrayImageResolution=72 \
+    -dMonoImageDownsampleType=/Bicubic \
+    -dMonoImageResolution=72 \
+    -sOutputFile="$1/$2" \
+     $TMPFILE
+
+ls -l "$2" "$1/$2"
