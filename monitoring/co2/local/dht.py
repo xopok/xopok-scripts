32e03ef3a9cdc35c85833f73b46f8e97e82ad55e
Vitaly Lysenkov <vlysenkov@gmail.com>
2020-01-17 23:11:04 +0100

    dht.py now keeps the previous value and doesn't produce the new value if it is too far from the previous. Helps with unstable connections to sensors

--- monitoring/co2/local/dht.py
+++ monitoring/co2/local/dht.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+import os
 import sys
 import Adafruit_DHT
 
@@ -13,12 +14,35 @@ if len(sys.argv) >= 2:
 else:
         sys.exit(1)
 
-while True:
-  humidity, temperature = Adafruit_DHT.read_retry(sensor_args['22'], pin)
+filename = os.path.join('/dev/shm', 'dht-' + pin)
+try:
+  with open(filename) as f:
+    h, t = [float(x) for x in next(f).split()]
+except:
+  h = None
+  t = None
+
+def get():
+  humidity, temperature = Adafruit_DHT.read_retry(sensor_args['22'], pin, retries=11)
   if humidity is not None and temperature is not None and 0 <= humidity <= 100.0:
-    print('{1:0.1f},{0:0.1f}'.format(temperature, humidity))
+    return (humidity, temperature)
+  return (None, None)
+
+if h is None:
+  # Need to seed the stored values
+  h, t = get()
+
+for i in (1,2,3,4):
+  h2, t2 = get()
+  if h is not None and 0 <= h <= 100.0 and h2 is not None and 0 <= h2 <= 100.0 and abs(t - t2) < 1:
+    # Good readings
+    with open(filename, "w+") as f:
+      print('{0:0.1f} {1:0.1f}'.format(h2, t2), file=f)
+    print('{0:0.1f} {1:0.1f}'.format(h2, t2))
     sys.exit(0)
   else:
-    print('Failed to get reading. Try again', file=sys.stderr)
+    print('Attempt {0:d} failed'.format(1), file=sys.stderr)
+        
 
+print('U U')
 sys.exit(1)
