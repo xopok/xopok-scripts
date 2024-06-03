80a1556d94b4c88c8b2368a3ee6f7bc37385a457
Vitaly Lysenkov <vlysenkov@gmail.com>
2024-06-03 16:05:54 +0200

    add installation notes to sdr.sh

--- monitoring/co2/local/sdr.sh
+++ monitoring/co2/local/sdr.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
+# How to install:
+# 1. Adjust the path to the sdr.py file below.
+# 2. Install the rtl-433 using apt, as usual.
+# 3. Install the service:
+#   a. `sudo ln -fs `realpath ./rtl_433.service` /etc/systemd/system/`
+#   b. `sudo service rtl_433 start`
+#   c. `sudo systemctl status rtl_433.service` - check that it's running.
+
 rtl_433 -F json 2>/dev/null | /home/vlysenkov/xopok-scripts/monitoring/co2/local/sdr.py
